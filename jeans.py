25fe3b44993a915a208a89e77b53da89ae579054
Peterson-peter <ptp_is@hotmail.com>
2018-04-19 16:45:23 -0700

    try that

--- jeans.py
+++ jeans.py
@@ -29,15 +29,15 @@ def write_emailfile(name, color, salePrices,OrgPrices,filename):
     with open(filename,"a") as emailfile:
         emailfile.write("<html><head></head><body><table>")
         emailfile.write("<tr><th>Name</th><th>Color</th><th>Sale Price</th><th>Org Price</th></tr>")
-        while z != len(salePrices):
-            if float(salePrices[z].split("$")[1]) / float(OrgPrices[z].split("$")[1]) < .50:
-                emailfile.write("<tr bgcolor='#FF0000'>")
-            elif float(salePrices[z].split("$")[1]) / float(OrgPrices[z].split("$")[1]) < .70:
-                emailfile.write("<tr bgcolor='#FFFF00'>")
-            elif float(salePrices[z].split("$")[1]) / float(OrgPrices[z].split("$")[1]) < .75:
-                emailfile.write("<tr bgcolor='#00FF00'>")
-            else:
-                emailfile.write("<tr>")
+        #while z != len(salePrices):
+        #    if float(salePrices[z].split("$")[1]) / float(OrgPrices[z].split("$")[1]) < .50:
+        #        emailfile.write("<tr bgcolor='#FF0000'>")
+        #    elif float(salePrices[z].split("$")[1]) / float(OrgPrices[z].split("$")[1]) < .70:
+        #        emailfile.write("<tr bgcolor='#FFFF00'>")
+        #    elif float(salePrices[z].split("$")[1]) / float(OrgPrices[z].split("$")[1]) < .75:
+        #        emailfile.write("<tr bgcolor='#00FF00'>")
+        #    else:
+        #        emailfile.write("<tr>")
             emailfile.write("<td>")
             emailfile.write(name[z])
             emailfile.write("</td>")
